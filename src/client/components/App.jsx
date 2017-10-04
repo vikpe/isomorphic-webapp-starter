3469ddb539c5bacf3ba92585a4d566559ee9e03c
Viktor Persson <viktor.persson@arcsin.se>
2017-10-04 22:26:43 +0200

    Re-organized files/folders.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 
-require("!style-loader!css-loader!sass-loader!./App.scss");
+require('!style-loader!css-loader!sass-loader!../assets/scss/App.scss');
 
-const reactLogo = require('./react_logo.svg');
+const reactLogo = require('../assets/img/react_logo.svg');
 
-class App extends React.Component {
-    render() {
-        return <div className="app">
-            <h1>Hello World!</h1>
-            <p>Foo to the bar</p>
-            <img src={reactLogo}/>
-        </div>;
-    }
+class App extends React.PureComponent {
+  render() {
+    return (
+        <div className="app">
+          <h1>Hello World!</h1>
+          <p>Foo to the bar</p>
+          <img src={reactLogo}/>
+        </div>
+    );
+  }
 }
 
 export default App;
--- src/client/index.js
+++ src/client/index.js
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppContainer} from "react-hot-loader";
-import App from "./components/App";
+import {AppContainer} from 'react-hot-loader';
+import App from './components/App';
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root');
 
 const renderComponent = (Component) => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        rootEl
-    );
+  ReactDOM.render(
+      <AppContainer>
+        <Component/>
+      </AppContainer>,
+      rootEl,
+  );
 };
 
 renderComponent(App);
 
 // Hot Module Replacement API
 if (module.hot) {
-    module.hot.accept("./components/App", () => {
-        const NewApp = require("./components/App").default;
-        renderComponent(NewApp);
-    });
+  module.hot.accept('./components/App', () => {
+    const NewApp = require('./components/App').default;
+    renderComponent(NewApp);
+  });
 }

d22d3f98145a45958274c9e7843d02a042dcdb12
Viktor Persson <viktor.persson@arcsin.se>
2017-10-04 23:50:12 +0200

    Updated comments in webpack client dev config.

--- configs/webpack/client.dev.js
+++ configs/webpack/client.dev.js
@@ -6,19 +6,19 @@ const commonClientConfig = require('./client.common');
 
 module.exports = merge(commonClientConfig, {
   entry: [
-    'react-hot-loader/patch', // activate HMR for React
+    'react-hot-loader/patch', // HMR for React
     'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
     'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './client/index.jsx' // the entry point of our app
+    './client/index.jsx' // app entry point
   ],
   devServer: {
     hot: true, // enable HMR on the server
-    contentBase: commonClientConfig.output.path, // match the output path
-    publicPath: commonClientConfig.output.publicPath // match the output `publicPath`
+    contentBase: commonClientConfig.output.path,
+    publicPath: commonClientConfig.output.publicPath,
   },
   devtool: 'cheap-module-eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), // enable HMR globally
-    new webpack.NamedModulesPlugin() // prints more readable module names in the browser console on HMR updates
+    new webpack.HotModuleReplacementPlugin(), // HMR
+    new webpack.NamedModulesPlugin() // prints readable module names in the browser console on HMR updates
   ],
 });

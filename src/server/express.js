5d543b7ae2f94e154851fedbc42fca87496b5f4f
Viktor Persson <viktor.persson@arcsin.se>
2017-10-07 15:35:13 +0200

    Added compression (express middleware) to enableoutput compression.

--- src/server/express.js
+++ src/server/express.js
@@ -1,11 +1,13 @@
-const chalk      = require('chalk');
-const express    = require('express');
-const app        = express();
-const portNumber = 3000;
-const sourceDir  = 'dist/public';
+const chalk       = require('chalk');
+const express     = require('express');
+const compression = require('compression');
+const app         = express();
+const portNumber  = 3000;
+const publicDir   = 'dist/public';
 
-app.use(express.static(sourceDir));
+app.use(compression()); // compress all responses
+app.use(express.static(publicDir)); // serve static content 
 
 app.listen(portNumber, () => {
-  console.log(chalk`{green.bold Server started:} {white http://localhost:${portNumber}} {grey (/${sourceDir}/)}`);
+  console.log(chalk`{green.bold Server started:} {white http://localhost:${portNumber}} {grey (/${publicDir}/)}`);
 });

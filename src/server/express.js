3d0dcb77a07e12113e3ba99b1073bb79277334c5
Viktor Persson <viktor.persson@arcsin.se>
2017-10-07 19:23:40 +0200

    Added body parser middleware for express.

--- src/server/express.js
+++ src/server/express.js
@@ -1,11 +1,13 @@
 import chalk from 'chalk';
 import express from 'express';
+import bodyParser from 'body-parser';
 import compression from 'compression';
 
 const app        = express();
 const portNumber = 3000;
 const publicDir  = 'dist/public';
 
+app.use(bodyParser.json()); // Parse incoming request bodies as JSON (available under request.body)
 app.use(compression()); // compress all responses
 app.use(express.static(publicDir)); // serve static content 
 

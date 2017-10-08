a7b139db4eb51aff8193eef47af3cb98e4063c26
Viktor Persson <viktor.persson@arcsin.se>
2017-10-08 15:16:01 +0200

    Added "react-helmet" (document head manager), eg for changing title.

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import '!style-loader!css-loader!sass-loader!../assets/scss/app.scss';
 import reactLogo from '../assets/img/react_logo.svg';
 import TodoListContainer from './../containers/TodoListContainer';
+import Helmet from 'react-helmet';
 
 class App extends React.PureComponent {
   render() {
     return (
         <div className="app">
+          <Helmet>
+            <title>Isomorphic Webapp Starter</title>
+          </Helmet>
           <img src={reactLogo}/>
           <h1>Isomorphic Webapp Starter</h1>
           <p>Isomorphic web application starter with hot module replacement (HMR) for rapid development.</p>

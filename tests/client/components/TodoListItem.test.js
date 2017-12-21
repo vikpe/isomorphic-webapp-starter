38b6f0f9b185a95f926954b5ce76eb73b6ebb3ad
Viktor Persson <viktor.persson@arcsin.se>
2017-12-21 13:39:41 +0100

    Updated test for TodoListItem.

--- tests/client/components/TodoListItem.test.js
+++ tests/client/components/TodoListItem.test.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
 import TodoListItem from 'client/components/TodoListItem';
 
 describe('<TodoListItem />', () => {
   test('renders', () => {
-    const item = {
-      title: 'Alpha',
+    const props = {
+      item: { title: 'Alpha' },
+      onDeleteClick: sinon.spy(),
     };
-    const div = document.createElement('div');
-    ReactDOM.render(<TodoListItem item={item} />, div);
+    const wrapper = shallow(<TodoListItem {...props} />);
+
+    expect(wrapper.find('div.app-todo-list-item').length).toEqual(1);
+    expect(wrapper.find('a').length).toEqual(1);
+    expect(
+      wrapper
+        .find('div')
+        .first()
+        .text(),
+    ).toContain('Alpha');
+  });
+
+  test('triggers onDeleteClick', () => {
+    const props = {
+      item: { title: 'Alpha' },
+      onDeleteClick: sinon.spy(),
+    };
+    const wrapper = shallow(<TodoListItem {...props} />);
+    wrapper
+      .find('a')
+      .first()
+      .simulate('click');
+    sinon.assert.calledOnce(props.onDeleteClick);
   });
 });
-

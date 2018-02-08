d70be7e6f1769faab44bef0f07d22bb135516d69
Viktor Persson <viktor.persson@arcsin.se>
2018-02-08 19:01:56 +0100

    Applied prettier code reformatting to rootReducer test.

--- tests/client/reducers/rootReducer.test.js
+++ tests/client/reducers/rootReducer.test.js
@@ -1,16 +1,15 @@
 import rootReducer from 'client/reducers/rootReducer';
-import {initialState as itemsInitialState} from 'client/reducers/itemReducer';
+import { initialState as itemsInitialState } from 'client/reducers/itemReducer';
 
 describe('rootReducer', () => {
   test('initial state', () => {
     const initialState = undefined;
-    const action = {type: 'DEFAULT_CASE'};
+    const action = { type: 'DEFAULT_CASE' };
     const resultState = rootReducer(initialState, action);
     const expectedState = {
-      items: itemsInitialState
+      items: itemsInitialState,
     };
 
     expect(resultState).toEqual(expectedState);
   });
 });
-
